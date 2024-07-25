d81ba0254e93d5c21f9bfa81c11f9022661ff087
dimparar <77003325+dimparar@users.noreply.github.com>
2024-07-25 12:26:53 +0300

    test release

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dimparar/test
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,8 @@
+// main package is the entry point of the program
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
